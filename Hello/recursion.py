4157430a63d11d8dc37f085a060b655a5fca7ac2
Siphesihle Yapi <siphe2009@gmail.com>
2019-03-22 00:16:04 +0200

    Update recursion.py

--- Hello/recursion.py
+++ Hello/recursion.py
@@ -16,13 +16,13 @@ def fibonacci(n):
         return(fibonacci(n-1) + fibonacci(n-2))
 
 def factorial(n):
-    if n == 1:
-        return n
-    else:
-        lower_fact = factorial(n-1)
-        current_fact = n * lower_fact
+    '''Return n!'''
+    x = 1
+    y = 2
 
-    return  current_fact
+    while y <= n:
+        x = x * y
+        y = y + 1
 
 def reverse(word):
 
